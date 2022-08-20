401e68d7a09d5db65e1d7e400e608b4afbb0edec
Tatiana <amora4ka@gmail.com>
2022-08-20 12:43:02 +0300

    feat: insert registration to html page

--- RSLang/src/app/index.ts
+++ RSLang/src/app/index.ts
@@ -1,6 +1,7 @@
 import { IUserInfo } from '../api/interfaces';
 import RegistrationController from '../controller/registration';
 import { clearForm, collectNewUserInfo } from '../view/forms';
+import showHideModal from '../view/modal';
 import IApp from './interfaces';
 
 class App implements IApp {
@@ -12,7 +13,12 @@ class App implements IApp {
 
   public start() {
     const registerBtn = document.querySelector('#register-user-btn');
+    const registrationModal = document.querySelector<HTMLElement>('#registration-modal');
+    const registerBtns = document.querySelectorAll('.registration__regbtn');
+    const blackout = document.querySelector<HTMLElement>('.blackout');
 
+    blackout?.addEventListener('click', () => showHideModal(registrationModal as HTMLElement, blackout));
+    registerBtns.forEach((btn) => btn?.addEventListener('click', () => showHideModal(registrationModal as HTMLElement, blackout as HTMLElement)));
     registerBtn?.addEventListener('click', (e) => {
       e.preventDefault();
       const registrateForm = document.querySelector<HTMLFormElement>('#create-user-form');
--- RSLang/src/index.ts
+++ RSLang/src/index.ts
@@ -1,5 +1,3 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min';
 import './style.scss';
 import App from './app';
 
