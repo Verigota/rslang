2251ba5a003c4da9af2255737e80de05720d9bb1
beaJenius <beajenius@yandex.ru>
2022-08-23 01:39:04 +0300

    feat: add audio game start view template

---
+++ RSLang/src/view/views/audiogame.ts
@@ -0,0 +1,16 @@
+export const sectionAudioGameStart = `
+            <section class="audio-game content__item">
+                <div class="audio-game__about">
+                    <h2 class="audio-game__title">Аудиовызов</h2>
+                    <p class="audio-game__desc">Тренировка Аудиовызов улучшает восприятие речи на слух.</p>
+                </div>
+                <div class="audio-game__help">
+                    <p class="audio-game__help-item">Чтобы играть с помощью клавиатуры, используйте клавиши:</p>
+                    <p class="audio-game__help-item">1, 2, 3, 4, 5 - чтобы дать ответ,</p>
+                    <p class="audio-game__help-item">space - для воспроизведения звука,</p>
+                    <p class="audio-game__help-item">enter - чтобы пропустить вопрос,</p>
+                    <p class="audio-game__help-item">стрелка-вправо - чтобы перейти к следующему вопросу.</p>
+                </div>
+            </section>`;
+
+export const sectionAudioGameBody = '';
