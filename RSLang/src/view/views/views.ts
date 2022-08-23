f927b2958282f1679f372caff0cd10c6db0d8db9
beaJenius <beajenius@yandex.ru>
2022-08-23 23:17:59 +0300

    feat: add view names

--- RSLang/src/view/views/views.ts
+++ RSLang/src/view/views/views.ts
@@ -1,25 +1,28 @@
 import { ViewType } from './viewType';
-import {
-  sectionAbout, sectionScope, sectionTeam, sectionRegistration,
-} from './main';
+import { sectionAbout, sectionScope, sectionTeam } from './main';
 import {
   sectionGames, sectionPaginationTop, sectionDictionary, sectionPaginationBottom,
 } from './handbook';
 import sectionGamesChoice from './gameschoice';
-import { sectionAudioGameStart } from './audiogame';
+import { sectionAudioGameStart, sectionAudioGameBody } from './audiogame';
+import { popupMessage } from './popup';
 
 export const mainView: ViewType = {
-  sections: [sectionAbout, sectionScope, sectionTeam, sectionRegistration],
+  sections: [sectionAbout, sectionScope, sectionTeam],
+  name: 'main',
 };
 
 export const handbookView: ViewType = {
   sections: [sectionGames, sectionPaginationTop, sectionDictionary, sectionPaginationBottom],
+  name: 'handbook',
 };
 
 export const gamesChoiceView: ViewType = {
   sections: [sectionGamesChoice],
+  name: 'gamechoice',
 };
 
 export const gameAudioStart: ViewType = {
   sections: [sectionAudioGameStart],
+  name: 'audiogame-start',
 };
