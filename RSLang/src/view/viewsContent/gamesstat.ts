b62b7612a217e774e6572afbeaf7f133c8f59fb0
beaJeniuS <beajenius@yandex.ru>
2022-08-26 21:55:49 +0300

    feat: add game stat view

---
+++ RSLang/src/view/viewsContent/gamesstat.ts
@@ -0,0 +1,47 @@
+export const popupGameStatView = `
+                <div class="popup__content game-stat">
+                    <a href="#header" class="popup__close">
+                        <img src="./public/assets/icons/cross.svg" alt="close">
+                    </a>
+                    <div class="game-stat__about">
+                        <h2 class="game-stat__title">Аудиовызов</h2>
+                        <p class="game-stat__result">Ваш результат <span class="game-stat__result-val">250</span></p>
+                        <p class="game-stat__serie">Длина серии: <span class="game-stat__serie-val">10</span></p>
+                    </div>
+                    <div class="game-stat__container">
+                        <div class="game-stat__slider">
+                            <div id="game-stat-common" class="game-stat__item">
+                                <div class="game-stat__percent">50%</div>
+                            </div>
+                            <div id="game-stat-answers" class="game-stat__item">
+                                <p class="stat-header">Ошибки: <span class="game-stat__errors">10</span></p>
+                                <div class="game-stat__errors-words"></div>
+                                <div class="stat-separator"></span></div>
+                                <p class="stat-header">Правильные: <span class="game-stat__rights">5</span></p>
+                                <div class="game-stat__rights-words"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="game-stat__pagination">
+                        <a id="game-stat-left" href="#" class="game-stat__pag-btn"></a>
+                        <a id="game-stat-right" href="#" class="game-stat__pag-btn"></a>
+                    </div>
+                    <div class="game-stat__action">
+                        <button class="button game-stat__restart">Cнова</button>
+                        <button class="button game-stat__game-select">Игры</button>
+                    </div>
+                </div>`;
+
+export const gameStatWord = `
+                        <div class="game-stat__el">
+                            <a href="#" class="game-stat__el-play">
+                                <img src="./public/assets/icons/play.svg" alt="play">
+                                <p class="game-stat__el-word"></p>
+                                <p class="game-stat__el-dash">-</p>
+                                <p class="game-stat__el-transl"></p>
+                            </a>
+                        </div>`;
+
+export const gameStatAnswersSeparator = '<div class="stat-separator"></span></div>';
+export const gameStatAnswersErrors = '<p class="stat-header">Ошибки: <span class="game-stat__errors"></span></p>';
+export const gameStatAnswersRights = '<p class="stat-header">Правильные: <span class="game-stat__rights"></span></p>';
