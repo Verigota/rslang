53bb0d24e388bc70fa04898399ee672163bc7807
beaJeniuS <beajenius@yandex.ru>
2022-08-28 14:57:23 +0300

    feat: add GameController base class

--- RSLang/src/controller/games/GameController.ts
+++ RSLang/src/controller/games/GameController.ts
@@ -1,9 +1,11 @@
 import { herokuApi } from '../../api';
 import { IApi } from '../../api/interfaces';
+import { WordDataT, WordsDataT } from '../../types/types';
 
 interface IGameStart {
   level: string;
   page: string;
+  words: WordsDataT | [];
 }
 
 export default class GameController {
