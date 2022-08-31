4cabcc146dda502a1bfd078e1f0cef4ed1212f29
beaJeniuS <beajenius@yandex.ru>
2022-08-31 23:45:01 +0300

    feat: chabge the way to starting message popup

--- RSLang/src/view/gameSelector/commonFunc.ts
+++ RSLang/src/view/gameSelector/commonFunc.ts
@@ -1,36 +1,4 @@
-import GameAudioCallStartView from '../games/audiocall/audioCallStartView';
-import { popupMsg } from '../viewsContent/views';
-
-export function showMessage(title: string, message: string) {
-  let popup = document.querySelector('#message') as HTMLDivElement;
-  const body = document.querySelector('body') as HTMLElement;
-  if (popup === null) {
-    popup = document.createElement('div');
-    popup.classList.add('popup');
-    popup.setAttribute('id', 'message');
-    popup.innerHTML = popupMsg.sections.join('');
-    popup.classList.add('open');
-    body.append(popup);
-  } else {
-    popup.classList.add('open');
-  }
-  const popupTitle = popup.querySelector('#popup-title') as HTMLHeadingElement;
-  const popupMessage = popup.querySelector('.popup__message') as HTMLParagraphElement;
-  popupTitle.innerText = title;
-  popupMessage.innerText = message;
-
-  const popupArea = popup.querySelector('.popup__area') as HTMLAnchorElement;
-  const popupClose = popup.querySelector('.popup__close') as HTMLAnchorElement;
-  const popupCloseBtn = popup.querySelector('.popup__close-btn') as HTMLButtonElement;
-  const buttons = [popupArea, popupClose, popupCloseBtn];
-  buttons.forEach((el) => {
-    el.addEventListener('click', () => {
-      popup.classList.remove('open');
-    });
-  });
-}
-
-function checkLevelsBtns(): boolean {
+export function checkLevelsBtns(): boolean {
   const levelBtns = document.querySelectorAll('.choice__level');
   return (Array.prototype.slice.call(levelBtns).some((el) => el.classList.contains('selected')));
 }
@@ -47,14 +15,5 @@ export function setGamesButtonsActions() {
         el.classList.add('selected');
       }
     });
-  });
-  const audioGameBtn = document.querySelector('#audio-game') as HTMLAnchorElement;
-  audioGameBtn.addEventListener('click', () => {
-    if (checkLevelsBtns()) {
-      const gameAudioCall = new GameAudioCallStartView();
-      gameAudioCall.render();
-    } else {
-      showMessage('Не выбран уровень сложности!', 'Выберите уровень сложности, чтобы продолжить!');
-    }
   });
 }
